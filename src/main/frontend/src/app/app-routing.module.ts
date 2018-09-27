09a6e3280852ff932ea09b704887eb49b85b6baf
Albert Cieslak <albert.cieslak2@gmail.com>
2018-09-27 23:39:18 +0200

    small changes

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {OrderDishComponent} from "./order-dish/order-dish.component";
+import {AppComponent} from "./app.component";
+import {MenuComponent} from "./menu/menu.component";
 
 const routes: Routes = [
-  { path: 'order/:dishId', component: OrderDishComponent },
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+  { path: 'order/:id', component: OrderDishComponent },
 ];
 
 @NgModule({
