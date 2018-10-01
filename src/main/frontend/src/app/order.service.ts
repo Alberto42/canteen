730b45eec74b60d0c72e2ce2db94e281b1e6fd59
Albert Cieslak <albert.cieslak2@gmail.com>
2018-10-01 16:32:48 +0200

    Make order (call webservice) and redirect

---
+++ src/main/frontend/src/app/order.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {RequestOptions} from "@angular/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(private http: HttpClient) { }
+
+  order(dishId : string, seat : number) {
+    return this.http.post(`/api/order?dishId=${dishId}&seat=${seat}`,null).subscribe(
+      (error) => console.log(error)
+    );
+  }
+}
